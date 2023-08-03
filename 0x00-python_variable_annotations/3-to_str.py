7efac710b93d9d27670374fc2e0994e758d99a05
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 02:49:27 +0300

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Module for task 3 - Basic annotations - to string
+"""
+
+def to_str(n: float) -> str:
+    """
+    Function that takes a float n as argument and
+    returns the string representation of the float.
+    """
+    return str(n)
