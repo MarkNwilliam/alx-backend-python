f28b2b1a586b62ec3276a4e68edbce967b9fe0d9
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 02:41:18 +0300

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module for task 0 - Basic annotations - add
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Function that takes two floats (a and b) as arguments and returns their sum.
+    """
+    return a + b
