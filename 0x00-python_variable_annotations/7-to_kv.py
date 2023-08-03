c122fa41730d498fe02abaaa36aafbdf88c6e1d0
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 02:53:13 +0300

    Create 7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for task 7 - Complex types - string and int/float to tuple
+"""
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Function that takes a string and an int or float.
+    Returns a tuple where the first element is the string
+    and the second is the square of the int/float.
+    """
+    return (k, float(v**2))
