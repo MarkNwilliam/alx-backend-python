2d11262333e108b32d12f4622a340a80ccfd0825
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 02:51:40 +0300

    Create 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Module for task 5 - Complex types - list of floats
+"""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Function that takes a list of floats as an argument
+    and returns the sum of the list.
+    """
+    return sum(input_list)
