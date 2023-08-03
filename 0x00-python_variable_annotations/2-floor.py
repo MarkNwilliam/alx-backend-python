93f5c2005b75b5cbd6f6d05a2861bcfbdba16f80
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 02:48:14 +0300

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for task 2 - Basic annotations - floor
+"""
+
+def floor(n: float) -> int:
+    """
+    Function that takes a float n as argument and
+    returns the floor of the float.
+    """
+    return int(n // 1)
