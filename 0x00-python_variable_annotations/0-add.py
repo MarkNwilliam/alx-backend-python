319c9544e5ebae1b2f4587404bcffae7f555adb9
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 10:00:47 +0300

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 """
-  Module for task 0 - Basic annotations - add
+Module for task 0 - Basic annotations - add
 """
 
+# Import statements here
+
+
 def add(a: float, b: float) -> float:
-     '''
-        Returns sum of two floats.
-    '''
+    """
+    Function that takes two float arguments and returns their sum as a float.
+    """
     return a + b
