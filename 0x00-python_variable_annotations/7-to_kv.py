4df51f84903520f9767e514c9b058f3f0930de48
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 10:15:49 +0300

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,6 +5,7 @@ Module for task 7 - Complex types - string and int/float to tuple
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Function that takes a string and an int or float.
