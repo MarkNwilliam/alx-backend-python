bea5c46c8d7f9ddbd4b15629c78be89f8061939f
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 10:03:41 +0300

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,6 +5,7 @@ Module for task 1 - Basic annotations - concat
 """
 
 
+
 def concat(str1: str, str2: str) -> str:
     """
     Function that takes two string arguments and returns their concatenated string.
