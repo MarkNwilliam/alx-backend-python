c940ecf2b21354338cf55eab8a1672b65840a723
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 10:33:08 +0300

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+
 """
 Module for task 10 - Duck typing - first element of a sequence
 """
 
-from typing import Sequence, Any, Union, NoneType
+from typing import Sequence, Any, Union, Optional
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, Optional]:
     """
-    Returns the first element of a sequence or None if the sequence is empty.
+    Get the value of a key in a dictionary safely.
     """
     if lst:
         return lst[0]
     else:
         return None
+
