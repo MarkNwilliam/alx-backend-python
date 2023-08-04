d9bd739687f0a05d0475ff0f297cac6f9fd9fd7c
Nkugwa Mark William <53256592+MarkNwilliam@users.noreply.github.com>
2023-08-04 10:16:01 +0300

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,6 +6,7 @@ Module for task 8 - Complex types - functions
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Function that returns another function that multiplies a given float by multiplier.
